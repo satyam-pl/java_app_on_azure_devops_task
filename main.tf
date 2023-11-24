73bab67fd46ea1dcf2b5ea5a5f39afef51e30d74
Satyam Patel <115926436+satyam-pl@users.noreply.github.com>
2023-11-24 12:14:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,9 +18,11 @@ provider "azurerm" {
   features {}
 
   subscription_id = "fafa58df-5f3a-4423-91ba-552d42b280dd"
-  client_id       = "db6d1d0e-9246-4297-9e2a-cd9e5f7120e9"
-  client_secret   = "c9c41253-efbb-4ca2-9a82-efc0d3882eaa"
-  tenant_id       = "ce3685eb-9b4e-4652-a942-7ea5cda9e6c0"  
+  client_id       = "eeceac2a-308b-4295-a715-08f24fe03707"
+  client_secret   = "4e0ded0a-7bf4-4747-ab51-f5ce73776b6e"
+  tenant_id       = "ce3685eb-9b4e-4652-a942-7ea5cda9e6c0"
+
+  version = "=2.0"
 }
 
 resource "azurerm_resource_group" "example" {
