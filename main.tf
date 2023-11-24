fa95f9aefbe1f5132503bd6a74a55db797e9973e
Satyam Patel <115926436+satyam-pl@users.noreply.github.com>
2023-11-24 11:53:01 +0530

    Update main.tf
    changing client id

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ provider "azurerm" {
   features {}
 
   subscription_id = "fafa58df-5f3a-4423-91ba-552d42b280dd"
-  client_id       = "QSr8Q~u3CzOOngmyTDKuSzor4p~N2HFpC535Ga3Y"
+  client_id       = "db6d1d0e-9246-4297-9e2a-cd9e5f7120e9"
   client_secret   = "c9c41253-efbb-4ca2-9a82-efc0d3882eaa"
   tenant_id       = "ce3685eb-9b4e-4652-a942-7ea5cda9e6c0"
 
