96b375d344b5ee8f0e20ccbf3f3dad718a01fee7
Satyam Patel <115926436+satyam-pl@users.noreply.github.com>
2023-11-24 15:33:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,13 +16,6 @@ terraform {
 }
 provider "azurerm" {
   features {}
-
-  subscription_id = "fafa58df-5f3a-4423-91ba-552d42b280dd"
-  client_id       = "e12a2d0e-c7a8-497a-9c6a-f6feb27b10ac"
-  client_secret   = "~Ik8Q~u1dpSqwite8M6b-y4doLRhIEZPVpcmFcx4"
-  tenant_id       = "ce3685eb-9b4e-4652-a942-7ea5cda9e6c0"
-
-  version = "=2.0"
 }
 
 resource "azurerm_resource_group" "example" {
