d2b67d039dd8e40a8ce0024e413019d5a86218f4
Satyam Patel <115926436+satyam-pl@users.noreply.github.com>
2023-11-24 16:34:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,13 @@
 terraform {
   required_providers {
-    snowflake = {
-      source  = "chanzuckerberg/snowflake"
-      version = "0.25.17"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=2.0"
     }
   }
 
   backend "remote" {
     organization = "satyam-patel-trident"
-
     workspaces {
       name = "gh-actions-demo-java"
     }
