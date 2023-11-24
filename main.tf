212f843155c8fe463bc4b7bca610779e44090317
Satyam Patel <115926436+satyam-pl@users.noreply.github.com>
2023-11-24 14:42:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,8 +18,8 @@ provider "azurerm" {
   features {}
 
   subscription_id = "fafa58df-5f3a-4423-91ba-552d42b280dd"
-  client_id       = "eeceac2a-308b-4295-a715-08f24fe03707"
-  client_secret   = "4e0ded0a-7bf4-4747-ab51-f5ce73776b6e"
+  client_id       = "e12a2d0e-c7a8-497a-9c6a-f6feb27b10ac"
+  client_secret   = "e12a2d0e-c7a8-497a-9c6a-f6feb27b10ac"
   tenant_id       = "ce3685eb-9b4e-4652-a942-7ea5cda9e6c0"
 
   version = "=2.0"
