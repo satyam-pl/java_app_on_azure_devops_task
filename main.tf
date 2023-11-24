36434034a8c00610d62d6bcf579b97d8b5cf9977
Satyam Patel <115926436+satyam-pl@users.noreply.github.com>
2023-11-24 17:02:27 +0530

    Update main.tf
    first commit of main.tf

--- main.tf
+++ main.tf
@@ -27,3 +27,26 @@ resource "azurerm_resource_group" "example" {
   name     = "satyam-github-demo"
   location = "East US"
 }
+
+resource "azurerm_app_service" "example_webapp" {
+  name                = "my-java-app"  # Specify your Azure Web App name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id  = azurerm_app_service_plan.example.id
+
+  site_config {
+    java_version = "1.8"  # Specify the Java version
+    java_container = "TOMCAT"  # Specify the Java container
+  }
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "my-app-service-plan"  # Specify your App Service Plan name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
